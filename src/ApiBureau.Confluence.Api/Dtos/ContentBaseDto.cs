0dc9742a4e10be1776d2a2471434a325a82b1690
Vaclav Elias <vaclavelias@gmail.com>
2022-01-22 19:31:50 +0000

    ContentBase Added

---
+++ src/ApiBureau.Confluence.Api/Dtos/ContentBaseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiBureau.Confluence.Api.Dtos
+{
+    public class ContentBaseDto
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Status { get; set; } = null!;
+    }
+}
--- src/ApiBureau.Confluence.Api/Dtos/ContentDto.cs
+++ src/ApiBureau.Confluence.Api/Dtos/ContentDto.cs
@@ -1,13 +1,12 @@
 namespace ApiBureau.Confluence.Api.Dtos
 {
-    public class ContentDto
+    public class ContentDto : ContentBaseDto
     {
-        public int Id { get; set; }
         public int SpaceId { get; set; }
-        public string Type { get; set; } = null!;
-        public string Title { get; set; } = null!;
         public BodyDto Body { get; set; } = default!;
         public VersionDto? Version { get; set; }
         public SpaceDto? Space { get; set; }
+
+        public List<ContentBaseDto> Ancestors { get; set; } = new();
     }
 }
