0b963ce19b45ece563c5421b3326336ace1f5865
Vaclav Elias <vaclavelias@gmail.com>
2022-01-22 19:53:07 +0000

    Ancestor added to expand

--- src/ApiBureau.Confluence.Api/Core/SpaceExpand.cs
+++ src/ApiBureau.Confluence.Api/Core/SpaceExpand.cs
@@ -2,18 +2,24 @@ namespace ApiBureau.Confluence.Api.Core;
 
 public class SpaceExpand
 {
-    private string _expandText = string.Empty;
+    private List<string> _items = new();
 
-    public string Get() => string.IsNullOrWhiteSpace(_expandText) ? "" : "?expand=" + _expandText;
+    public string Get() => _items.Count == 0 ? "" : "&expand=" + string.Join(",", _items);
 
     public SpaceExpand AddVersion() => AddItem("version");
 
-    public SpaceExpand AddBody(string type = "body") => AddItem("storage." + type);
+    public SpaceExpand AddAncestors() => AddItem("ancestors");
+
+    /// <summary>
+    /// Body can be view or storage
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public SpaceExpand AddBody(string type = "view") => AddItem("body." + type);
 
     public SpaceExpand AddItem(string text)
     {
-
-        _expandText += text + ",";
+        _items.Add(text);
 
         return this;
     }
