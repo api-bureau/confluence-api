bf4b27c94c7079b584247ae47e97ab26f11bf5f2
Vaclav Elias <vaclavelias@gmail.com>
2022-02-14 17:33:26 +0000

    ContentdDto attachment corrected

--- src/ApiBureau.Confluence.Api/Dtos/ContentDto.cs
+++ src/ApiBureau.Confluence.Api/Dtos/ContentDto.cs
@@ -6,21 +6,21 @@ namespace ApiBureau.Confluence.Api.Dtos
         public BodyDto Body { get; set; } = default!;
         public VersionDto? Version { get; set; }
         public SpaceDto? Space { get; set; }
-
         public List<ContentBaseDto<int>> Ancestors { get; set; } = new();
         public ChildrenDto? Children { get; set; }
     }
 
-    public class ChildrenDto {
-        public AttachmentResultDto? Attachment { get; set; }
-    }
-
-    public class AttachmentResultDto
+    public class ChildrenDto
     {
-        public ResultDto<AttachmentDto>? Results { get; set; }
+        public ResultDto<AttachmentDto>? Attachment { get; set; }
     }
 
     public class AttachmentDto : ContentBaseDto<string>
+    {
+        public ExtensionDto? Extensions { get; set; }
+    }
+
+    public class ExtensionDto
     {
         public string MediaType { get; set; } = null!;
     }
