ede7a4c2dadd6cc0524be1917e5b169e6049a5e4
Vaclav Elias <VaclavElias@gmail.com>
2022-03-03 09:16:32 +0000

    PropertyDto added

---
+++ src/ApiBureau.Confluence.Api/Dtos/PropertyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiBureau.Confluence.Api.Dtos;
+
+public class PropertyDto
+{
+    public string Id { get; set; } = null!;
+    public string Key { get; set; } = null!;
+    public string Value { get; set; } = null!;
+}
