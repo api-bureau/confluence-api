ad16ddb90fc5d47574f29daf4c96af9972862515
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-10-11 18:32:04 +0100

    feat!: ConfluenceClient - Add BlogPostEndpoint property
    - Added a new property `BlogPost` of type `BlogPostEndpoint` to the `ConfluenceClient` class.
    - Updated the `ConfluenceClient` constructor to initialize the `BlogPost` property with a new instance of `BlogPostEndpoint`, passing the `apiConnection` parameter to its constructor.

--- src/ApiBureau.Confluence.Api/ConfluenceClient.cs
+++ src/ApiBureau.Confluence.Api/ConfluenceClient.cs
@@ -5,12 +5,14 @@ namespace ApiBureau.Confluence.Api;
 public class ConfluenceClient
 {
     public AttachmentEndpoint Attachment { get; }
+    public BlogPostEndpoint BlogPost { get; }
     public ContentEndpoint Content { get; }
     public SpaceEndpoint Spaces { get; }
 
     public ConfluenceClient(ApiConnection apiConnection)
     {
         Attachment = new AttachmentEndpoint(apiConnection);
+        BlogPost = new BlogPostEndpoint(apiConnection);
         Content = new ContentEndpoint(apiConnection);
         Spaces = new SpaceEndpoint(apiConnection);
     }
